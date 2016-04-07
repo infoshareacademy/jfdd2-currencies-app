d46a6c797f9c9bdce8967acceeb828263f1b4e7e
agnieszkaberka <agnberka@gmail.com>
2016-04-07 22:13:18 +0200

    Green currency Icon on Click.

--- src/app/js/liked.js
+++ src/app/js/liked.js
@@ -1,3 +1,14 @@
-$(board).on('click', 'div', function () {
+$('.iconDiv').click(function () {
+    var $this = $(this);
+    var $childOfDiv = $(this).children();
 
-})
+    if($this.hasClass('liked')){
+        $this.removeClass('liked');
+        $childOfDiv.removeClass('liked');
+    }
+    else{
+        $this.addClass('liked');
+        $childOfDiv.addClass('liked');
+    }
+
+});
