27f22ad5766f57e14145337ca160445b880db105
lukaszsmola <lukasz.smola@agnat.pl>
2016-04-07 15:33:18 +0200

    wykresy kursow.

--- src/app/app.js
+++ src/app/app.js
@@ -11,10 +11,10 @@
     app.controller("LineCtrl", function ($scope) {
 
         $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.series = ['Series A', 'Series B'];
+        $scope.series = ['Series A'];
         $scope.data = [
-            [100, 59, 80, 81, 56, 55, 40],
-            [0, 48, 40, 19, 86, 27, 90]
+            [100, 59, 80, 81, 56, 55, 40]
+
         ];
         $scope.onClick = function (points, evt) {
             console.log(points, evt);
