a84012b08a319e5abdb460b12298363f0c2ad777
agnieszkaberka <agnberka@gmail.com>
2016-04-08 09:21:41 +0200

    Max 3 liked currency

--- src/app/js/liked.js
+++ src/app/js/liked.js
@@ -1,14 +1,18 @@
 $('.iconDiv').click(function () {
     var $this = $(this);
     var $childOfDiv = $(this).children();
+    var $likedCurr = $('.liked').length / 3;
 
-    if($this.hasClass('liked')){
-        $this.removeClass('liked');
-        $childOfDiv.removeClass('liked');
-    }
-    else{
+    if(!$this.hasClass('liked') && $likedCurr < 3){
+
         $this.addClass('liked');
         $childOfDiv.addClass('liked');
     }
+    else{
+
+        $this.removeClass('liked');
+        $childOfDiv.removeClass('liked');
+    }
 
 });
+
