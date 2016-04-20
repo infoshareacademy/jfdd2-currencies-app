12e795fb0e19e7a689d387d3cd312324eafeffa3
agnieszkaberka <agnberka@gmail.com>
2016-04-20 10:10:38 +0200

    Creating user object (with login, liked currencies and friends recommends ), which is saved in local storage

--- src/app/js/googlePlus.js
+++ src/app/js/googlePlus.js
@@ -7,13 +7,21 @@ function onSuccess(googleUser) {
     $('#backcover').css({'display': 'none'});
     $('#loginWindow').css({'display': 'none'});
 
+    var userObject =  {
+                 login: googleUser.getBasicProfile().getEmail(),
+                 likedCurrency: JSON.parse(localStorage.getItem('likedCurrencyObject')) || {},
+                 friendsRecommend: JSON.parse(localStorage.getItem('friendsRecommendationObject')) || {}
+             };
+
+           localStorage.setItem('UserObject', JSON.stringify(userObject));
 
 
-    if (googleUser.getBasicProfile().getName() === localStorage.getItem('name') && localStorage.getItem('socialMedia')=== 'Google +') {
-        var $recommendAsNamePara = $("<p>").text('Your friend ' + localStorage.getItem('name') + ' recommended you our application');
-        $('#recommendAsName').append($recommendAsNamePara);
-        $('#recommendAsNameDiv').show();
-    }
+//TODO po zrobieniu wysylania na serwer
+    //if (googleUser.getBasicProfile().getName() === localStorage.getItem('name') && localStorage.getItem('socialMedia')=== 'Google +') {
+    //    var $recommendAsNamePara = $("<p>").text('Your friend ' + localStorage.getItem('name') + ' recommended you our application');
+    //    $('#recommendAsName').append($recommendAsNamePara);
+    //    $('#recommendAsNameDiv').show();
+    //}
 }
 function onFailure(error) {
     console.log(error);
