023ce3ba2f7b9b130c4ef4bc484b72b243c95362
agnieszkaberka <agnberka@gmail.com>
2016-04-22 10:48:09 +0200

    Show alert, when someone recommended our app to login user.

--- src/app/ajax-client/client.js
+++ src/app/ajax-client/client.js
@@ -3,167 +3,47 @@ serverStorage = {
   url: 'http://isa-api-sl.herokuapp.com/api',
 
 
-getUserObject: function() {
-      return $.ajax({
-          type: 'GET',
-          url: this.url + '/userData',
-          dataType: 'json',
-          success: function(result) {
-              console.log('get user information from server');
-              console.log(result);
-          },
-          error: function(error) {
-              console.log('error send user ');
-          }
-      });
+  getUserObject: function () {
+    return $.ajax({
+      type: 'GET',
+      url: this.url + '/userData',
+      dataType: 'json',
+      success: function (result) {
+        console.log('get user information from server');
+        console.log(result);
+      },
+      error: function (error) {
+        console.log('error send user ');
+      }
+    });
   },
-  addUserObject: function(person, id) {
+  addUserObject: function (person, id) {
     $.ajax({
       type: 'PUT',
       url: this.url + '/userData/' + id,
-      contentType:'application/json',
+      contentType: 'application/json',
       data: person,
-      success: function (result){
+      success: function (result) {
         console.log(result);
       },
-      error: function (result){
+      error: function (result) {
         console.error(result);
       }
     })
   },
-  addNewUserObject: function(person) {
+  addNewUserObject: function (person) {
     $.ajax({
       type: 'POST',
-      url: this.url + '/userData/' ,
-      contentType:'application/json',
+      url: this.url + '/userData/',
+      contentType: 'application/json',
       data: person,
-      success: function (result){
+      success: function (result) {
         console.log(result);
       },
-      error: function (result){
+      error: function (result) {
         console.error(result);
       }
     })
   },
 
-
-
-
-
-
-  //EXAMPLES
-
-
-
-  //getInsurance: function() {
-  //    $.ajax({
-  //        type: 'GET',
-  //        url: this.url + '/insurance',
-  //        dataType: 'text',
-  //        success: function(result) {
-  //            var wiersze = result.split('\r')    ;
-  //            var wierszNaglowkowy = wiersze[0].split(',');
-  //            var obiekty = [];
-  //
-  //            wiersze.forEach(function(wiersz, i) {
-  //                if (i == 0) return;
-  //                var nowyObiekt = {};
-  //                wiersz.split(',').forEach(function(wartosc, indexWartosci) {
-  //                    var nazwaPolaWObiekcie = wierszNaglowkowy[indexWartosci];
-  //                    nowyObiekt[nazwaPolaWObiekcie] = wartosc;
-  //                });
-  //                obiekty.push(nowyObiekt)
-  //            });
-  //
-  //            console.log(obiekty);
-  //        }
-  //    })
-  //},
-
-
-  //getBooks: function(callback) {
-  //    $.ajax({
-  //        type: 'GET',
-  //        url: this.url + '/books',
-  //        dataType: 'xml',
-  //        success: function(result) {
-  //            console.info(result);
-  //            callback(result);
-  //        },
-  //        error: function(error) {
-  //            console.error(error);
-  //        }
-  //    });
-  //},
-
-  //getPeople: function(callback, queryData) {
-  //    $.ajax({
-  //        type: 'GET',
-  //        url: this.url + '/people',
-  //        data: queryData,
-  //        dataType: 'json',
-  //        success: function(result) {
-  //            console.info(result);
-  //            callback(result);
-  //        }
-  //    });
-  //},
-  //
-  //
-  //getBooksWithPromise: function() {
-  //  return $.ajax({
-  //    type: 'GET',
-  //    url: this.url + '/books',
-  //    dataType: 'xml'
-  //  });
-  //},
-  //
-  //getCompaniesWithPromise: function() {
-  //  return $.ajax({
-  //    type: 'GET',
-  //    url: this.url + '/companies',
-  //    dataType: 'json'
-  //  });
-  //},
-  //
-  //getPeopleWithPromise: function(queryData) {
-  //  return $.ajax({
-  //    type: 'GET',
-  //    url: this.url + '/people',
-  //    data: queryData,
-  //    dataType: 'json'
-  //  });
-  //},
-
-
-  //runPromise: function() {
-  //
-  //    $.when(serverStorage.getPeopleWithPromise(), serverStorage.getBooksWithPromise())
-  //        .done(function(people, books) {
-  //            console.debug(people, books);
-  //        })
-  //        .fail(function(error) {
-  //            console.error(error);
-  //        })
-  //        .always(function(){
-  //            console.log('always method');
-  //        })
-//  //},
-//
-//
-//  addPerson: function(person) {
-//    $.ajax({
-//      type: 'POST',
-//      url: this.url + '/people/add',
-//      contentType:'application/json',
-//      data: JSON.stringify(person),
-//      success: function (result){
-//        console.log(result);
-//      },
-//      error: function (result){
-//        console.error(result);
-//      }
-//    })
-//
-//  }
 };
--- src/app/js/googlePlus.js
+++ src/app/js/googlePlus.js
@@ -8,47 +8,50 @@ function onSuccess(googleUser) {
   $('#loginWindow').css({'display': 'none'});
 
   serverStorage.getUserObject().done(function (userFromServer) {
-    var userExist = userFromServer.filter(function(item){
-       return item.login == googleUser.getBasicProfile().getEmail();
+    var userExist = userFromServer.filter(function (item) {
+      return item.login == googleUser.getBasicProfile().getEmail();
     });
 
-    if(userExist.length === 0){
+    if (userExist.length === 0) {
       var newUser = {
         login: googleUser.getBasicProfile().getEmail(),
-        likedCurrency:  {},
-        friendsRecommend:  {}
+        likedCurrency: {},
+        friendsRecommend: {}
       };
       serverStorage.addNewUserObject(JSON.stringify(newUser));
-    }else {
+    } else {
 
       var userId = [];
       userExist.forEach(function (item) {
         return userId.push(item.id);
       });
 
+
       var userObject = {
         login: googleUser.getBasicProfile().getEmail(),
-        likedCurrency: JSON.parse(localStorage.getItem('likedCurrencyObject')) || {},
-        friendsRecommend: JSON.parse(localStorage.getItem('friendsRecommendationObject')) || {}
+        likedCurrency: JSON.parse(localStorage.getItem('likedCurrencyObject')) || userExist.likedCurrency || {},
+        friendsRecommend: JSON.parse(localStorage.getItem('friendsRecommendationObject')) || userExist.friendsRecommend || {}
       };
 
       localStorage.setItem('UserObject', JSON.stringify(userObject));
-      console.log(userId[0]);
-
       serverStorage.addUserObject(JSON.stringify(userObject), userId[0]);
     }
 
-    //serverStorage.getUserObject();
+
+    userFromServer.forEach(function (item) {
+      //Add div when someone recommend our app to login user
+      item.friendsRecommend.email.forEach(function (item2) {
+        if (item2 === googleUser.getBasicProfile().getEmail() && item.login !== googleUser.getBasicProfile().getEmail()) {
+          var $recommendAsNamePara = $("<p>").text('Your friend  recommended You our application from email: ' + item.login);
+          $('#recommendAsName').append($recommendAsNamePara);
+          $('#recommendAsNameDiv').show();
+          return console.log(item.id);
+        }
+      });
+    });
+
   });
 
-
-
-//TODO po zrobieniu wysylania na serwer
-  //if (googleUser.getBasicProfile().getName() === localStorage.getItem('name') && localStorage.getItem('socialMedia')=== 'Google +') {
-  //    var $recommendAsNamePara = $("<p>").text('Your friend ' + localStorage.getItem('name') + ' recommended you our application');
-  //    $('#recommendAsName').append($recommendAsNamePara);
-  //    $('#recommendAsNameDiv').show();
-  //}
 }
 function onFailure(error) {
   console.log(error);
