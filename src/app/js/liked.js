fc7d4800744476684efc06b3aa3066607cd24c0a
agnieszkaberka <agnberka@gmail.com>
2016-04-22 11:11:37 +0200

    Revert "Features/ohc 87"

--- src/app/js/liked.js
+++ src/app/js/liked.js
@@ -1,45 +1,45 @@
 var likedCurrencyObject = JSON.parse(localStorage.getItem('likedCurrencyObject')) || {
-    name: []
+      name: []
   };
 
-function setNewCurrency(name) {
-  likedCurrencyObject.name.push(name);
+function setNewCurrency(name){
+    likedCurrencyObject.name.push(name);
 }
 
-if (likedCurrencyObject.name.length <= 3) {
-  likedCurrencyObject.name.forEach(function (item) {
-    $("#" + item).addClass('liked');
-    $("#" + item).children().addClass('liked');
-
-  });
-} else {
-  localStorage.removeItem('likedCurrencyObject');
+if(likedCurrencyObject.name.length <= 3) {
+    likedCurrencyObject.name.forEach(function (item) {
+        $("#" + item).addClass('liked');
+        $("#" + item).children().addClass('liked');
+        console.log($("#" + item));
+    });
+}else{
+    localStorage.removeItem('likedCurrencyObject');
 }
 $('div.iconDiv').click(function () {
 
-  var $this = $(this);
-  var $childOfDiv = $(this).children();
-  var $likedCurr = $('.liked').length / 3;
+    var $this = $(this);
+    var $childOfDiv = $(this).children();
+    var $likedCurr = $('.liked').length / 3;
 
 
-  if (!$this.hasClass('liked') && $likedCurr < 3) {
+    if (!$this.hasClass('liked') && $likedCurr < 3) {
 
-    setNewCurrency($this.attr('id'));
-    $this.addClass('liked');
-    $childOfDiv.addClass('liked');
-    localStorage.setItem('likedCurrencyObject', JSON.stringify(likedCurrencyObject));
-  }
-  else {
-    $this.removeClass('liked');
-    $childOfDiv.removeClass('liked');
-    var unCancelLiked = likedCurrencyObject.name.filter(function (item) {
-      return item !== $this.attr('id');
-    });
-    likedCurrencyObject.name = unCancelLiked;
-    localStorage.setItem('likedCurrencyObject', JSON.stringify(likedCurrencyObject));
+          setNewCurrency($this.attr('id'));
+          $this.addClass('liked');
+          $childOfDiv.addClass('liked');
+          localStorage.setItem('likedCurrencyObject', JSON.stringify(likedCurrencyObject));
+      }
+      else {
+          $this.removeClass('liked');
+          $childOfDiv.removeClass('liked');
+          var unCancelLiked = likedCurrencyObject.name.filter(function (item) {
+              return item !== $this.attr('id');
+          });
+          likedCurrencyObject.name = unCancelLiked;
+          localStorage.setItem('likedCurrencyObject', JSON.stringify(likedCurrencyObject));
 
-  }
-});
+      }
+  });
 
 
 
